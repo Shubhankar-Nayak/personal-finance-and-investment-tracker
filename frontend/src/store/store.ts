36ebacc353eef3c5c3d749bbfc50c01766fcb7b0
Shubhankar-Nayak <shubhankarnayak05@gmail.com>
2025-07-26 00:16:27 +0530

    updated store.ts

--- frontend/src/store/store.ts
+++ frontend/src/store/store.ts
@@ -2,12 +2,14 @@ import { configureStore } from '@reduxjs/toolkit';
 import authReducer from './slices/authSlice';
 import transactionReducer from './slices/transactionSlice';
 import budgetReducer from './slices/budgetSlice';
+import investmentReducer from './slices/investmentSlice'
 
 export const store = configureStore({
   reducer: {
     auth: authReducer,
     transactions: transactionReducer,
     budgets: budgetReducer,
+    investments: investmentReducer,
   },
 });
 
