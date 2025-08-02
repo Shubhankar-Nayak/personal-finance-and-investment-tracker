e889dfdbe65fd4ed39776f4bb6fa27bf001755d4
Shubhankar-Nayak <shubhankarnayak05@gmail.com>
2025-08-02 20:36:42 +0530

    access control changes

--- backend/server.ts
+++ backend/server.ts
@@ -17,11 +17,16 @@ connectDB();
 const app = express();
 
 app.use(cors({
-  origin: ['http://localhost:5173', 'https://personal-finance-and-investment-tracker-l0020fsmg.vercel.app'],
+  origin: ['http://localhost:5173', 'https://personal-finance-and-investment-tra.vercel.app'],
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true
 }));
+
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Credentials', 'true');
+  next();
+});
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }))
