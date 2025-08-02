a549f9eafd8d28c10a88a8f263b03b055d967921
Shubhankar-Nayak <shubhankarnayak05@gmail.com>
2025-08-02 17:07:27 +0530

    server changes

--- backend/server.ts
+++ backend/server.ts
@@ -9,26 +9,27 @@ import budgetRoutes from './routes/budgetRoutes';
 import userRoutes from './routes/userRoutes';
 import authRoutes from './routes/authRoutes';
 
+import path from 'path';
+
 dotenv.config();
 connectDB();
 
 const app = express();
 
 app.use(cors({
-  origin: 'http://localhost:5173', 
+  origin: process.env.FRONTEND_URL || 'http://localhost:5173', 
   credentials: true, 
 }));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }))
 
-
-const PORT = process.env.PORT || 5000;
-
 app.use('/api/transactions', transactionRoutes);
 app.use('/api/investments', investmentRoutes);
 app.use('/api/budget', budgetRoutes);
 app.use('/api/user', userRoutes);
 app.use('/api/auth', authRoutes);
+
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
